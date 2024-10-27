316b61e6d075ef04b4d33aad907eddd8ca26fb04
rledford <ryan.c.ledford@gmail.com>
2024-10-27 12:51:33 -0500

    Update config required env vars.

--- src/platform/config/config.schema.ts
+++ src/platform/config/config.schema.ts
@@ -2,10 +2,10 @@ import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
   PORT: Joi.number().integer().min(0).max(65535).default(8900),
-  MONGO_HOST: Joi.string(),
+  MONGO_HOST: Joi.string().default('localhost'),
   MONGO_PORT: Joi.number().integer().min(1).max(65535).default(27017),
-  MONGO_USER: Joi.string(),
-  MONGO_PASS: Joi.string(),
-  MONGO_NAME: Joi.string(),
-  GNL_CLIENT_API_KEY: Joi.string(),
+  MONGO_USER: Joi.string().required(),
+  MONGO_PASS: Joi.string().required(),
+  MONGO_NAME: Joi.string().required(),
+  GNL_CLIENT_API_KEY: Joi.string().required(),
 });
